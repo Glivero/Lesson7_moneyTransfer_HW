742e3afc19b9c4abfd32458b4cc73c54b67c94f0
Denis <denisermakov1986@gmail.com>
2020-09-20 10:21:25 +0300

    Initial commit

---
+++ src/main/java/com/example/moneytransfer/service/MoneyTransferService.java
@@ -0,0 +1,31 @@
+package com.example.moneytransfer.service;
+
+import com.example.moneytransfer.model.Account;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MoneyTransferService {
+
+    Lock locker = new ReentrantLock();
+    Condition condition = locker.newCondition();
+
+    public void transfer(Account from, Account to, Long amount) throws InterruptedException {
+        locker.lock();
+        try {
+            while (from.getBalance() < amount) {
+                System.out.println(String.format("Недостаточно денег!", from.getId(), amount - from.getBalance()));
+                condition.await();
+            }
+            from.setBalance(from.getBalance() - amount);
+            to.setBalance(to.getBalance() + amount);
+        } finally {
+            condition.signalAll();
+            locker.unlock();
+        }
+    }
+
+
+
+}
---
+++ src/test/java/com/example/moneytransfer/MoneyTransferApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.moneytransfer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoneyTransferApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
